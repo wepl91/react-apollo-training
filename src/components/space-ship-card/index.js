cb4b12eb3ac517ae4b04bc77903ae4f6d50f237b
Walter Pereyra Lopez <walter.pereyra@mercadolibre.com>
2021-01-15 22:01:08 -0300

    Ships cards style

--- src/components/space-ship-card/index.js
+++ src/components/space-ship-card/index.js
@@ -9,23 +9,21 @@ const SectionCard = ({ spaceShip, history }) => (
   <div className="space-ship-card">
     <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
       <Image src={spaceShip.image} alt={'space-ship'} />
-      <Box p="6">
+      <Box
+        mt="1"
+        fontWeight="semibold"
+        as="h4"
+        lineHeight="tight"
+        isTruncated
+      >
+        {`Nombre: ${spaceShip.name}`}
         <Box
           mt="1"
-          fontWeight="semibold"
-          as="h4"
+          as="h6"
           lineHeight="tight"
           isTruncated
         >
-          {`Nombre: ${spaceShip.name}`}
-        </Box>
-        <Box
-          mt="1"
-          as="h4"
-          lineHeight="tight"
-          isTruncated
-        >
-          {`Nombre: ${spaceShip.type}`}
+          {`Tipo: ${spaceShip.type}`}
         </Box>
       </Box>
     </Box>
